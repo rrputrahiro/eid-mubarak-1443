11eec696b42bde85a22ac111ed65d3b7d67ed496
rrputrahiro <rrputrafn@gmail.com>
2022-08-13 21:33:27 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  theme: {
+    extend: {
+      fontFamily: {
+        almarai: ["Almarai"],
+        poppins: ["Poppins"],
+      },
+    },
+  },
+  plugins: [],
+};
